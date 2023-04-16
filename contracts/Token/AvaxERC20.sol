f24353bfd959017362f8d01739c8e01b8eb643c0
zicoz18 <zicoz18@ku.edu.tr>
2023-04-16 10:31:45 +0300

    Optimize AvaxERC20 and fix logic error about `_decimals`

--- contracts/Token/AvaxERC20.sol
+++ contracts/Token/AvaxERC20.sol
@@ -5,14 +5,13 @@ import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
 // A standard ERC20 token with maxSupply of 1 million
 contract AvaxERC20 is ERC20 {
-  uint public MAX_SUPPLY = 1000000 ether;
+  uint public constant MAX_SUPPLY = 1000000 ether;
 
   // maxSupply is sent to the creator of the token
   constructor(
     string memory _name,
-    string memory _symbol,
-    uint8 _decimals
-  ) ERC20(_name, _symbol, _decimals) {
+    string memory _symbol
+  ) ERC20(_name, _symbol, 18) {
     _mint(msg.sender, MAX_SUPPLY);
   }
 }
