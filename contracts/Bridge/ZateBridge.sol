b7f4664c9a9837490af2091f71c7f29154483f46
zicoz18 <zicoz18@ku.edu.tr>
2023-04-16 11:05:17 +0300

    Improve ZateBridge

--- contracts/Bridge/ZateBridge.sol
+++ contracts/Bridge/ZateBridge.sol
@@ -4,31 +4,38 @@ pragma solidity ^0.8.7;
 import "../Token/INativeMinter.sol";
 
 contract SubnetBridge {
-  address public admin;
+  /* Selector to call the mintNativeCoin function of INativeMinter */
+  bytes4 private constant NATIVE_MINT_SELECTOR =
+    bytes4(keccak256(bytes("mintNativeCoin(address,uint256)")));
   /* Address to send tokens to burn them */
-  address public burnAddress = address(0x0);
+  address public constant burnAddress = address(0x0);
+
+  address public constant nativeMinter =
+    address(0x0200000000000000000000000000000000000001);
+
+  /* Admin of the contract that is able to call `release()` */
+  address public admin;
+
   /* Gets incremented with each `burn()`, indicates the transferCount
     and prevents double processing the event */
   uint public nonce;
 
   /* Represents NativeMinterInterface */
-  NativeMinterInterface public nativeMinter =
-    NativeMinterInterface(
-      /*
-                Native Minter contract is always at this address
-                as explained at https://docs.avax.network/subnets/customize-a-subnet#minting-native-coins
-             */
-      address(0x0200000000000000000000000000000000000001)
-    );
 
   /* Mapping to hold whether nonce is processed or not */
-  mapping(uint => bool) public processedNonces;
+  mapping(uint => bool) public nonceToIsProcessed;
 
   /* Allows us to indicate whether it is a `mint()` or `burn()` when emitting an event */
   enum Type {
     Mint,
     Burn
   }
+
+  error InvalidBurnAmount(uint amount);
+  error NonceAlreadyProcessed(uint nonce);
+  error NativeMintFail();
+  error BurnFail();
+  error NotAdmin(address caller);
 
   /*
         Event that is emitted with both `mint()` and `burn()`
@@ -46,8 +53,14 @@ contract SubnetBridge {
 
   /* Modifier to allow some functions to be only called by admin */
   modifier onlyAdmin() {
-    require(msg.sender == admin, "only admin");
+    _onlyAdmin();
     _;
+  }
+
+  function _onlyAdmin() private view {
+    if (msg.sender != admin) {
+      revert NotAdmin(msg.sender);
+    }
   }
 
   /* Constructor that sets admin as the sender */
@@ -60,12 +73,31 @@ contract SubnetBridge {
     admin = newAdmin;
   }
 
+  function _safeNativeMint(address to, uint value) private {
+    (bool success, ) = nativeMinter.call(
+      abi.encodeWithSelector(NATIVE_MINT_SELECTOR, to, value)
+    );
+    if (!success) {
+      revert NativeMintFail();
+    }
+  }
+
+  function _safeBurn() private {
+    (bool success, ) = payable(burnAddress).call{value: msg.value}("");
+    if (!success) {
+      revert BurnFail();
+    }
+  }
+
   /* Function that is called by the relayer to mint some tokens after it is locked on the avax */
   function mint(address to, uint amount, uint avaxNonce) external onlyAdmin {
-    require(processedNonces[avaxNonce] == false, "nonce already processed");
-    processedNonces[avaxNonce] = true;
+    if (nonceToIsProcessed[avaxNonce]) {
+      revert NonceAlreadyProcessed(avaxNonce);
+    }
+    nonceToIsProcessed[avaxNonce] = true;
 
-    nativeMinter.mintNativeCoin(to, amount);
+    _safeNativeMint(to, amount);
+
     emit Transfer(
       msg.sender,
       to,
@@ -82,15 +114,19 @@ contract SubnetBridge {
         it will call `release()` of the AvaxBridge
     */
   function burn(address to) external payable {
-    require(msg.value > 0, "You have to burn more than 0 tokens");
+    if (msg.value == 0) {
+      revert InvalidBurnAmount(msg.value);
+    }
+
     /* Send native token to 0x0 address, effectively burning native token */
-    (bool sent, ) = payable(burnAddress).call{value: msg.value}("");
-    require(sent, "Failed to send native token");
+    _safeBurn();
 
     /* Event that is emitted for relayer to process */
     emit Transfer(msg.sender, to, msg.value, block.timestamp, nonce, Type.Burn);
 
     /* Increment the nonce to prevent double counting */
-    nonce++;
+    unchecked {
+      ++nonce;
+    }
   }
 }
