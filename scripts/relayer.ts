6b2d19df2a053dfea03a8420d17d4e98b0ec5b8c
zicoz18 <zicoz18@ku.edu.tr>
2023-05-20 11:43:39 +0300

    Update relayer to pre-check events

--- scripts/relayer.ts
+++ scripts/relayer.ts
@@ -1,21 +1,10 @@
-// TODO: Connect to both networks
-// TODO: Listen to events from both bridge contracts
-// TODO: Whenever
-
-import { TransferEvent } from '../typechain-types/contracts/Bridge/AvaxBridge'
-import { IBridgeTransfer, ITransaction } from './interfaces'
+import { TIME_IN_BETWEEN_TXS } from '../constants'
+import { ITransaction } from './interfaces'
 import { getBridgeContracts } from './utils/initBridgeContracts'
 import { getProviders } from './utils/initProviders'
 import { getSigners } from './utils/initSigners'
-import registerWorker from './worker'
 
-// TODO: Start listening to events
-
-const txs = [] as ITransaction[]
-
-// const processEvents = (logs: TransferEvent[]) => {
-//   logs.map((log) => log.args as unknown as IBridgeTransfer).map((transfer) => transfer.nonce)
-// }
+let txs = [] as ITransaction[]
 
 const main = async () => {
   // Connect to smart contracts on both blockchains
@@ -25,56 +14,76 @@ const main = async () => {
 
   const filter = await bridgeContracts.zate.filters.Transfer()
 
-  // const startFromBlockNumber = 0
-  // const latestBlockNumber = 25 // await providers.zate.getBlockNumber()
-  // const count = Math.floor(
-  //   (latestBlockNumber - startFromBlockNumber) / MAX_BLOCK_NUMBER_FOR_ONE_REQUEST
-  // )
-  // console.log(count)
-  // for (let i = 0; i < count; i++) {
-  //   console.log(`For i: ${i}`)
-  //   console.log(`Starts from: ${startFromBlockNumber + i * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST}`)
-  //   console.log(`Ends at: ${startFromBlockNumber + (i + 1) * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST}`)
-  //   const logs = await bridgeContracts.zate.queryFilter(
-  //     filter,
-  //     startFromBlockNumber + i * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST,
-  //     startFromBlockNumber + (i + 1) * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST
-  //   )
-  //   // processEvents(logs)
-  // }
-  // console.log(`Last iteration: `)
-  // console.log(`For i: ${count}`)
-  // console.log(`Starts from: ${startFromBlockNumber + count * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST}`)
-  // console.log(`Ends at: ${latestBlockNumber}`)
-  // const logs = await bridgeContracts.zate.queryFilter(
-  //   filter,
-  //   startFromBlockNumber + count * MAX_BLOCK_NUMBER_FOR_ONE_REQUEST,
-  //   latestBlockNumber
-  // )
-  // // processEvents(logs)
-
-  bridgeContracts.zate.on(filter, (from, to, amount, date, nonce, type) => {
-    console.log('zate running for an event')
-    console.log(from)
-    console.log(to)
-    console.log(amount.toString())
-    console.log(date.toString())
-    console.log(nonce.toString())
-    console.log(type.toString())
-    txs.push({ chain: 'avax', to, amount, nonce })
+  bridgeContracts.zate.on(filter, async (from, to, amount, date, nonce, type) => {
+    if (type === 1) {
+      console.log('zate burn event emitted')
+      const isProcessed = await bridgeContracts.avax.nonceToIsProcessed(nonce)
+      if (!isProcessed) {
+        txs.push({ chain: 'avax', to, amount, nonce })
+      } else {
+        console.log('already processed')
+      }
+    } else {
+      console.log('zate mint event emitted')
+    }
   })
 
-  bridgeContracts.avax.on(filter, (from, to, amount, date, nonce, type) => {
-    console.log('avax running for an event')
-    console.log(from)
-    console.log(to)
-    console.log(amount.toString())
-    console.log(date.toString())
-    console.log(nonce.toString())
-    console.log(type.toString())
-    txs.push({ chain: 'zate', to, amount, nonce })
+  bridgeContracts.avax.on(filter, async (from, to, amount, date, nonce, type) => {
+    if (type === 1) {
+      console.log('avax lock event emitted')
+      const isProcessed = await bridgeContracts.zate.nonceToIsProcessed(nonce)
+      if (!isProcessed) {
+        txs.push({ chain: 'zate', to, amount, nonce })
+      } else {
+        console.log('already processed')
+      }
+    } else {
+      console.log('avax release event emitted')
+    }
   })
 
-  registerWorker(txs, bridgeContracts)
+  setInterval(async () => {
+    console.log('worker ran')
+    console.log(txs)
+
+    if (txs.length > 0) {
+      // In case there is duplicate transactions, remove duplicates
+      txs = txs.filter((value, index) => {
+        const _value = JSON.stringify(value)
+        return (
+          index ===
+          txs.findIndex((obj) => {
+            return JSON.stringify(obj) === _value
+          })
+        )
+      })
+      if (txs.length > 0) {
+        const { chain, to, amount, nonce } = <ITransaction>txs.shift()
+        let tx
+        try {
+          if (chain === 'avax') {
+            console.log('sending transaction to fuji chain to release')
+            tx = await bridgeContracts.avax.release(to, amount, nonce)
+          } else if (chain === 'zate') {
+            console.log('sending transaction to zate chain to mint')
+            tx = await bridgeContracts.zate.mint(to, amount, nonce)
+          } else {
+            console.log('invalid transactions to process')
+            return
+          }
+          await tx.wait()
+          console.log('transaction included in the blockchain, token minted or release: ', {
+            chain,
+            to,
+            amount,
+            nonce,
+          })
+        } catch (err) {
+          console.log('error sending transaction: ', { chain, to, amount, nonce })
+          console.log(err)
+        }
+      }
+    }
+  }, TIME_IN_BETWEEN_TXS)
 }
 main()
--- scripts/worker.ts
+++ scripts/worker.ts
@@ -1,48 +1,59 @@
-import { ethers } from 'ethers'
-import { ITransaction } from './interfaces'
-import { AvaxBridge, ZateBridge } from '../typechain-types'
-import { TIME_IN_BETWEEN_TXS } from '../constants'
+// import { ethers } from 'ethers'
+// import { ITransaction } from './interfaces'
+// import { AvaxBridge, ZateBridge } from '../typechain-types'
+// import { TIME_IN_BETWEEN_TXS } from '../constants'
 
-const registerWorker = (
-  txs: ITransaction[],
-  bridgeContracts: {
-    avax: AvaxBridge
-    zate: ZateBridge
-  }
-) => {
-  setInterval(async () => {
-    console.log('worker ran')
-    if (txs.length > 0) {
-      // In case there is duplicate transactions, remove duplicates
-      txs = txs.filter((value, index) => {
-        const _value = JSON.stringify(value)
-        return (
-          index ===
-          txs.findIndex((obj) => {
-            return JSON.stringify(obj) === _value
-          })
-        )
-      })
-      if (txs.length > 0) {
-        const { chain, to, amount, nonce } = <ITransaction>txs.shift()
-        let tx
-        try {
-          if (chain === 'avax') {
-            tx = await bridgeContracts.avax.release(to, amount, nonce)
-          } else if (chain === 'zate') {
-            tx = await bridgeContracts.zate.mint(to, amount, nonce)
-          } else {
-            return
-          }
-          await tx.wait()
-          console.log('transaction minted, token minted or release: ', { chain, to, amount, nonce })
-        } catch (err) {
-          console.log('error sending transaction: ', { chain, to, amount, nonce })
-          console.log(err)
-        }
-      }
-    }
-  }, TIME_IN_BETWEEN_TXS)
-}
+// const registerWorker = (
+//   txs: ITransaction[],
+//   bridgeContracts: {
+//     avax: AvaxBridge
+//     zate: ZateBridge
+//   }
+// ) => {
+//   setInterval(async () => {
+//     console.log('worker ran')
+//     console.log(txs)
+//     if (txs.length > 0) {
+//       // In case there is duplicate transactions, remove duplicates
+//       txs = txs.filter((value, index) => {
+//         const _value = JSON.stringify(value)
+//         return (
+//           index ===
+//           txs.findIndex((obj) => {
+//             return JSON.stringify(obj) === _value
+//           })
+//         )
+//       })
+//       console.log('txs after deleting duplicates')
+//       console.log(txs)
+//       if (txs.length > 0) {
+//         const { chain, to, amount, nonce } = <ITransaction>txs.shift()
+//         let tx
+//         try {
+//           if (chain === 'avax') {
+//             console.log('sending transaction to fuji chain to release')
+//             tx = await bridgeContracts.avax.release(to, amount, nonce)
+//           } else if (chain === 'zate') {
+//             console.log('sending transaction to zate chain to mint')
+//             tx = await bridgeContracts.zate.mint(to, amount, nonce)
+//           } else {
+//             console.log('invalid transactions to process')
+//             return
+//           }
+//           await tx.wait()
+//           console.log('transaction included in the blockchain, token minted or release: ', {
+//             chain,
+//             to,
+//             amount,
+//             nonce,
+//           })
+//         } catch (err) {
+//           console.log('error sending transaction: ', { chain, to, amount, nonce })
+//           console.log(err)
+//         }
+//       }
+//     }
+//   }, TIME_IN_BETWEEN_TXS)
+// }
 
-export default registerWorker
+// export default registerWorker
